d6b0694d0aa7d7d3dd34714f9687ab3a8ed6dac5
12896 <12896@DESKTOP-T2RJ0KM>
2019-03-27 17:39:58 +0800

    第一次提交

---
+++ Plane/src/aeroplane/PassengerInterface.java
@@ -0,0 +1,8 @@
+package aeroplane;
+
+public interface PassengerInterface {
+public String  getName();//获取旅客姓名
+public int getBookingNum();//h获取预定号
+public int getrow();//获取座位排数
+public int getSeatPosition();//获取座位号
+}
